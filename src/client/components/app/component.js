9c5aa068724a6b7a904465af003ea8c6cbbae9a0
Jenna Rim <rim.jenna@yahoo.com>
2020-06-23 17:15:37 -0400

    Add day tracker styling and masonry post styling

--- src/client/components/app/component.js
+++ src/client/components/app/component.js
@@ -26,10 +26,30 @@ export default function App() {
                         </ul>
                     </nav>
                 </div>
-
             </div>
             <div>
+                <div className="day-tracker">
+                    <div className="day-number"> 7 </div>
+                    DAYS OF CONSECUTIVE KINDNESS
+                </div>
 
+                <div className="post-container">
+                    <div className="post">
+                        Cleaned up the streets with the whole neighborhood! #clean
+                    </div>
+                    <div className="post">
+                        Found a stray cat alone in an alleyway, and brought her to the local animal shelter where she's recovering
+                    </div>
+                    <div className="post">
+                        I helped volunteer at an organization that helps prevent fraud that targets senior citizens. I even was able to be a host for one of the sessions.
+                    </div>
+                    <div className="post">
+                        helped my mom with the groceries
+                    </div>
+                    <div className="post">
+                        today I gave a #donation to a cause I really care about
+                    </div>
+                </div>
                 <button onClick={() => setCount(count + 1)}>{count}</button>
             </div>
         </div>

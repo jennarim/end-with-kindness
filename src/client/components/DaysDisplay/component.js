f97579972acc52323fd513c2372ac9574334282a
Jenna Rim <rim.jenna@yahoo.com>
2020-07-10 22:38:16 -0400

    Day tracker counts up as in animation

--- src/client/components/DaysDisplay/component.js
+++ src/client/components/DaysDisplay/component.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CountUp from 'react-countup';
 
 export default class DaysDisplay extends React.Component {
     render() {
         return (
             <div className="day-tracker">
-                <div className="day-number"> {this.props.numberOfDays} </div>
+                <div className="day-number"> <CountUp end={this.props.numberOfDays} duration={2} /> </div>
                 DAYS OF CONSECUTIVE KINDNESS
             </div>
         );

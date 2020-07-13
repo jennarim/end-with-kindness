28bff8f410aa6a13c011490dda9fb97fb915b2bd
Jenna Rim <rim.jenna@yahoo.com>
2020-07-12 23:19:37 -0400

    Add feature to detect toxic language and prevent obscenities from being posted

--- src/client/components/ActForm/component.js
+++ src/client/components/ActForm/component.js
@@ -1,3 +1,4 @@
+/* global toxicity */
 import React from 'react';
 import axios from 'axios';
 import PropTypes from "prop-types";
@@ -8,28 +9,51 @@ const filter = new Filter();
 export default class ActForm extends React.Component {
     state = {
         content: '',
-        formValid: false
+        formValid: false,
+        isToxic: false
     };
 
     validateForm = () => {
-        const hasLength = this.state.content.length > 0;
-        const isClean = !filter.isProfane(this.state.content);
+        const content = this.state.content;
+        const hasLength = content.length > 0;
+        const isClean = !filter.isProfane(content);
         this.setState({ formValid: hasLength && isClean });
     }
 
     handleUserInput = event => {
-        this.setState({ content: event.target.value }, () => this.validateForm());
+        this.setState({ content: event.target.value }, this.validateForm);
+    }
+
+    inputIsToxic = () => {
+
     }
 
     handleSubmitAct = event => {
         event.preventDefault();
 
-        axios.post('/act', {
-            content: this.state.content
-        }).then(act => {
-            this.props.onSubmit(act.data);
-            this.setState({ content: '' });
+        const threshold = 0.9;
+        const labelsToInclude = ['identity_attack', 'insult', 'threat'];
+        toxicity.load(threshold, labelsToInclude).then(model => {
+            model.classify(this.state.content).then(predictions => {
+
+                const isToxic = predictions.some(prediction => prediction.results[0].match);
+                if (isToxic) {
+                    console.log("TOXIC CONTENT. NOT SUBMIITED");
+                    this.setState({ isToxic: true });
+                } else {
+                    this.setState({ isToxic: false }, () => {
+                        axios.post('/act', {
+                            content: this.state.content
+                        }).then(act => {
+                            this.props.onSubmit(act.data);
+                            this.setState({ content: '' });
+                        });
+                    });
+                }
+            });
         });
+
+
     }
 
     render() {
@@ -42,9 +66,19 @@ export default class ActForm extends React.Component {
                     value={this.state.content}
                     required>
                 </textarea>
+                <ToxicMessage isToxic={this.state.isToxic} />
                 <input type="submit" value="Submit" disabled={!this.state.formValid} />
             </form>
         );
+    }
+}
+
+function ToxicMessage(props) {
+    const isToxic = props.isToxic;
+    if (isToxic) {
+        return <div className="toxic-message"> This message was deemed too toxic. Please exercise respect when using this website. </div>
+    } else {
+        return <> </>;
     }
 }
 

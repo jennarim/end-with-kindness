488d3b63e07bbf3a38b4099d6378b8b6f3ccfa36
Jenna Rim <rim.jenna@yahoo.com>
2020-08-15 04:46:35 -0400

    Add database connection

---
+++ src/server/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+let dbconf;
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    dbconf = process.env.dbconf;
+} else {
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, 'config.json');
+
+    const data = fs.readFileSync(fn);
+    const conf = JSON.parse(data);
+
+    dbconf = conf.dbconf;
+}
+
+// mongoose.connect("mongodb://localhost/cdok", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbconf, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("DB connected.");
+    })
+    .catch(err => {
+        console.log("Error connceting to db", err);
+    });
